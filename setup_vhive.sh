7545ef32b9c06d37ebd41abb6535f7578d201ae1
dilinade <dilinade@users.noreply.github.com>
2023-10-09 02:48:47 +0100

    Add vhive & invitro

--- setup_vhive.sh
+++ setup_vhive.sh
@@ -1,1 +1,23 @@
 git clone --depth=1 https://github.com/vhive-serverless/vhive.git
+cd vhive
+mkdir -p /tmp/vhive-logs
+./scripts/cloudlab/setup_node.sh stock-only  # this might print errors, 
+ignore them
+sudo screen -d -m containerd
+./scripts/cluster/create_one_node_cluster.sh stock-only
+
+git clone --branch=words_sosp23_tutorial 
+https://github.com/vhive-serverless/invitro.git
+kubectl patch configmap/config-autoscaler -n knative-serving -p 
+'{"data":{"allow-zero-initial-scale":"true"}}'
+kubectl patch configmap/config-autoscaler -n knative-serving -p 
+'{"data":{"initial-scale":"0"}}'
+
+pushd invitro
+git lfs install
+git lfs fetch
+git lfs checkout
+sudo apt install pip
+pip install -r requirements.txt
+wget https://azurecloudpublicdataset2.blob.core.windows.net/azurepublicdatasetv2/azurefunctions_dataset2019/azurefunctions-dataset2019.tar.xz 
+-P ./data/azure
