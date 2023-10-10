1530857326fc48948ce4cfebc2a35dd419deb9f8
dilinade <dilinade@users.noreply.github.com>
2023-10-10 18:22:52 +0100

    Minor change

--- setup_vhive.sh
+++ setup_vhive.sh
@@ -1,8 +1,7 @@
 git clone --depth=1 https://github.com/vhive-serverless/vhive.git
 cd vhive
 mkdir -p /tmp/vhive-logs
-./scripts/cloudlab/setup_node.sh stock-only  # this might print errors, 
-ignore them
+./scripts/cloudlab/setup_node.sh stock-only  # this might print errors, ignore them
 sudo screen -d -m containerd
 ./scripts/cluster/create_one_node_cluster.sh stock-only
 
